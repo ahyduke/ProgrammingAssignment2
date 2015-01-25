49ada9034eb504e809d7daf45dbf747cd210e426
ahyduke <abigailrose.hyduke@gmail.com>
2015-01-25 14:02:49 -0500

    PA2 assigment ahyduke

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## create and cache inverse of a matrix.  Define functions to get and store values.  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #defined in workspace
+  inv<- NULL #cached inverse
+  
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  
+  get<-function()x
+  #assign function
+  setinv<-function(solve)inv<<-solve
+  getinv<-function()inv
+  #create a list of functions defined in makeCacheMatrix
+  list(set=set, get=get, 
+       setinv=setinv,
+       getinv=getinv)
 }
 
+##Return a matrix that is the inverse of 'x'
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinv()  
+  
+#searches for and gets value if currently stored
+
+  if(!is.null(inv))   {
+    message("getting cached data")
+#returns inverse of either computed or stored values   
+    return(inv)
+    
+  }
+  data<-x$get()
+  inv<-solve(data,...)
+  x$setinv(inv)
+  inv
 }
+  
+  
+  ## 
